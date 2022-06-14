86ce6092ae839f879a6fee1d1c658bd190cc4155
pisanzapra <pisanzapra@users.noreply.github.com>
2022-06-14 23:17:24 +0100

    Config update for Heroku (postgresql)

--- flasktodo/config.py
+++ flasktodo/config.py
@@ -3,8 +3,7 @@ from os import environ
 
 class Config:
 	SECRET_KEY = os.environ.get('FLASKTODO_SECRET_KEY')
-	SQLALCHEMY_DATABASE_URI = environ.get('FLASKTODO_DATABASE_URL')
-	#.replace("postgres://", "postgresql://", 1) or 'sqlite:///site.db'
+	SQLALCHEMY_DATABASE_URI = environ.get('FLASKTODO_DATABASE_URL').replace("postgres://", "postgresql://", 1) or 'sqlite:///site.db'
 	MAIL_SERVER = os.environ.get('MAIL_SERVER')
 	MAIL_PORT = os.environ.get('MAIL_PORT')
 	MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
